6f871968b30972b20ef55a2b2ddac63e936c63dc
Jaidev Nileshkumar K21054236 <jaidev.nileshkumar@kcl.ac.uk>
2022-10-15 13:00:14 +0100

    Add Thing model authentication

--- things/models.py
+++ things/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(models.Model):
-    name = models.CharField()
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30,unique=True, blank=False)
+    description = models.CharField(max_length=120, unique=False, blank=True)
+    quantity = models.IntegerField(unique=False, 
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100)
+        ])
